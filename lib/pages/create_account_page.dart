33dcb97aec1dd5b6a6d034e3c85483dcd0e16cea
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-09-08 10:43:07 -0500

    create page

--- lib/pages/create_account_page.dart
+++ lib/pages/create_account_page.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebaseconn2g9/constants/constants.dart';
+import 'package:firebaseconn2g9/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 
 class CreateAccountPage extends StatelessWidget {
   TextEditingController _correoController = TextEditingController();
   TextEditingController _contrasenaController = TextEditingController();
-
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  var logger = Logger(
+      // printer: PrettyPrinter(),
+      );
   Widget buildField(String titulo, TextEditingController controller) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -41,6 +47,44 @@ class CreateAccountPage extends StatelessWidget {
     );
   }
 
+  Future createAccount(BuildContext context) async {
+    try {
+      UserCredential userCredential =
+          await _auth.createUserWithEmailAndPassword(
+              email: _correoController.text,
+              password: _contrasenaController.text);
+      logger.d(userCredential);
+    } catch (error) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          backgroundColor: Colors.red,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25),
+          ),
+          behavior: SnackBarBehavior.floating,
+          content: Text(
+            // error.toString(),
+            mapErrorAuth(
+              error.toString(),
+            ),
+          ),
+        ),
+      );
+    }
+  }
+
+  String mapErrorAuth(String errorMessage) {
+    if (errorMessage.contains("email-already-in-use")) {
+      return "La dirección de correo ya esta en uso";
+    } else if (errorMessage.contains("invalid-email")) {
+      return "El correo no es válido";
+    } else if (errorMessage.contains("weak-password")) {
+      return "La contraseña no cumple con los estándares";
+    } else {
+      return "Ocurrió un problema al registrar la cuenta";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -75,6 +119,32 @@ class CreateAccountPage extends StatelessWidget {
                 ),
                 buildField("Correo electrónico", _correoController),
                 buildField("Contraseña", _contrasenaController),
+                SizedBox(
+                  height: 16,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (BuildContext context) => LoginPage(),
+                      ),
+                    );
+                  },
+                  child: Text(
+                    "o Inicia sesión",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                SizedBox(
+                  height: 32,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    createAccount(context);
+                  },
+                  child: Text("Crear cuenta"),
+                ),
               ],
             ),
           ),
