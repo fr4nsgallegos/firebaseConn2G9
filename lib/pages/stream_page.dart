3a438bf54386e9f046921ddb8d081d18eca2895c
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-09-08 09:22:58 -0500

    stream

--- lib/pages/stream_page.dart
+++ lib/pages/stream_page.dart
@@ -8,88 +8,67 @@ class StreamPage extends StatefulWidget {
 }
 
 class _StreamPageState extends State<StreamPage> {
-  late StreamController<int> streamController;
-
+  StreamController<int> myStreamController = StreamController();
   Future<int> getNumber() async {
     return 10;
   }
 
-  Stream<String> frutasStream() async* {
-    List<String> namesFrutas = ["pera", "manzana", "durazno", "melocoton"];
-    for (int i = 0; i < namesFrutas.length; i++) {
-      yield namesFrutas[i];
+  Stream<int> counter() async* {
+    for (int i = 0; i < 10; i++) {
+      yield i;
       await Future.delayed(Duration(seconds: 2));
     }
   }
 
-  void _startController() {
-    Stream<int> counter() async* {
-      for (int i = 0; i < 10; i++) {
-        yield i;
-        await Future.delayed(Duration(seconds: 2));
-      }
-    }
-
-    counter().listen(
-      (event) {
-        print(event);
-        streamController.sink
-            .add(event); //ENVIO LOS EVENTOS AL STREAM CONTROLLER
-      },
-      onError: (error) {
-        if (!streamController.isClosed) {
-          streamController.sink.addError(error);
-        }
-      },
-      onDone: () {
-        if (!streamController.isClosed) {
-          streamController.close();
-        }
-      },
-    );
-  }
-
   @override
   void initState() {
-    streamController = StreamController<int>();
-    _startController();
     super.initState();
   }
 
   @override
   void dispose() {
-    streamController.close();
     // TODO: implement dispose
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    getNumber().then((value) {
-      print(value);
+    // getNumber().then((value) {
+    //   print(value);
+    // });
+    print("init Stream");
+    Stream<int> myStream = Stream.fromFuture(getNumber());
+    print("STREAM CREADO");
+    myStream.listen((event) {
+      print(event);
     });
+    // counter().listen((value) {
+    //   print(value);
+    // }).onDone(() {
+    //   print("FINALIZADO");
+    // });
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
           title: Text("Stream Page"),
         ),
         //STREAM BUILDER
-        body: StreamBuilder(
-          stream: frutasStream(),
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (snapshot.hasData) {
-              return Center(
-                child: Text(
-                  snapshot.data.toString(),
-                  style: TextStyle(fontSize: 40),
-                ),
-              );
-            }
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          },
-        ),
+        // body: StreamBuilder(
+        //   stream: frutasStream(),
+        //   builder: (BuildContext context, AsyncSnapshot snapshot) {
+        //     if (snapshot.hasData) {
+        //       return Center(
+        //         child: Text(
+        //           snapshot.data.toString(),
+        //           style: TextStyle(fontSize: 40),
+        //         ),
+        //       );
+        //     }
+        //     return Center(
+        //       child: CircularProgressIndicator(),
+        //     );
+        //   },
+        // ),
 
         // //FUTURE BUILDER
         // body: FutureBuilder(
