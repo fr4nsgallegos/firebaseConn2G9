961103303b7066beb0d9875210cb72e2877c86a8
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-09-13 20:37:46 -0500

    personalizar marker

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,12 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
   Set<Marker> _markers = {};
+  BitmapDescriptor? _customMarker;
+
+  Future<void> _setCustomMarker() async {
+    _customMarker = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(
+          size: Size(100, 100),
+        ),
+        "assets/markers/orange.png");
+
+    _markers.add(
+      Marker(
+        markerId: MarkerId("002"),
+        position: LatLng(
+          -12.156056726143506,
+          -76.97245352927253,
+        ),
+        icon: _customMarker!,
+      ),
+    );
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _setCustomMarker();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        floatingActionButton: FloatingActionButton(onPressed: () {
+          _markers.forEach((e) => print(e));
+        }),
         body: GoogleMap(
           initialCameraPosition: CameraPosition(
             target: LatLng(
@@ -19,10 +55,13 @@ class MapPage extends StatelessWidget {
             print(position);
             _markers.add(
               Marker(
-                markerId: MarkerId("01"),
+                markerId: MarkerId(
+                  _markers.length.toString(),
+                ),
                 position: position,
               ),
             );
+            setState(() {});
           },
           markers: _markers,
           //  {
